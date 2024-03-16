ae24cbaa876c4122bc1d65b4f6220e46ba6b2abe
Spoorthib <130484941+01fe22bec229@users.noreply.github.com>
2024-03-16 20:50:50 +0530

    Add files via upload

---
+++ DSA_39.c
@@ -0,0 +1,30 @@
+//Program to print N terms of Fibbonacci series
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int prev=0,cur=1,i,n,temp;
+printf("Enter the number of terms you want to print \n");
+scanf("%d",&n);
+switch(n)
+{
+case 0 :printf("0 terms");
+        exit(0);
+case 1 :printf("%d",prev);
+        exit(0);
+case 2 : printf("%d   %d ",prev,cur);
+        exit(0);
+ default : break;
+}
+ printf("%d %d ",prev,cur);
+for(i=3;i<=n;i++)
+{
+    temp=prev;
+    prev=cur;
+     cur=prev+temp;
+
+    printf("%d  ",cur);
+
+}
+
+}
